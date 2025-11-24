b7e1682440d919dd862818528596db3f5d1f6a6f
jajiskoot <jajiskoot@yahoo.com>
2025-11-23 23:29:43 -0500

    change search input to form, so that key presses also work; install some shadcn components for asthetics; update some inline tailwind classes for example of other types of css changes

--- DISCUSSION.md
+++ DISCUSSION.md
@@ -4,7 +4,11 @@
 [] Other search params
    [] Greater than years of experience search param
    [] Devise best way to search specialties (check boxes?, separate input field?, how to deal with similar terms e.g. sleep vs nap, medicate vs medication)
+[] sort table columns
+[] paginate
 [] Fix layout
    [] intuitive search inputs
    [] easily readable results
    [] overall page structure
+   [] setup css loader
+   [] use/create component library (use storybook, ant, or shadcn)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,29 @@
 
 import { useEffect, useState } from "react";
 
+import {
+  TableHeader,
+  TableRow,
+  TableHead,
+  TableBody,
+  TableCell,
+  Table
+} from '@/components/ui/table';
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent
+} from '@/components/ui/card';
+import SolaceIcon from "@/components/svg/solace";
 import { Advocate } from "@/db/schema";
+
 
 export default function Home() {
   const [advocates, setAdvocates] = useState<Advocate[]>([]);
   const [searchTerm, setSearchTerm] = useState<string>("");
+  const [inputValue, setInputValue] = useState<string>("");
 
   const fetchAdvocates = (searchTerm = "") => {
     // paginate & pass search term query
@@ -21,64 +39,83 @@ export default function Home() {
     fetchAdvocates()
   }, []);
 
-  // debounce
-  const onChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
-    const searchTerm = e.target.value;
 
-    setSearchTerm(searchTerm);
+  const onChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setInputValue(e.target.value);
   };
 
-  const onClick = () => {
-    fetchAdvocates(searchTerm)
+  const handleReset = () => {
+    setSearchTerm("");
+    setInputValue("");
+    fetchAdvocates();
+  };
+
+  const handleSearch = () => {
+    fetchAdvocates(inputValue);
+    setSearchTerm(inputValue);
   };
 
   return (
-    <main style={{ margin: "24px" }}>
-      <h1>Solace Advocates</h1>
-      <br />
-      <br />
-      <div>
-        <p>Search</p>
-        <p>
-          Searching for: <span id="search-term">{searchTerm}</span>
-        </p>
-        <input style={{ border: "1px solid black" }} onChange={onChange} />
-        <button onClick={onClick}>Reset Search</button>
+    <main>
+      <div className="w-screen bg-[rgb(40,94,80)] px-6">
+        <SolaceIcon />
       </div>
-      <br />
-      <br />
-      <table>
-        <thead>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>City</th>
-            <th>Degree</th>
-            <th>Specialties</th>
-            <th>Years of Experience</th>
-            <th>Phone Number</th>
-          </tr>
-        </thead>
-        <tbody>
-          {advocates.map((advocate) => {
-            return (
-              <tr key={advocate.id}>
-                <td>{advocate.firstName}</td>
-                <td>{advocate.lastName}</td>
-                <td>{advocate.city}</td>
-                <td>{advocate.degree}</td>
-                <td>
-                  {advocate.specialties.map((s) => (
-                    <div>{s}</div>
-                  ))}
-                </td>
-                <td>{advocate.yearsOfExperience}</td>
-                <td>{advocate.phoneNumber}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <div className="mx-8">
+        <div className="my-10">
+          <h2>Search</h2>
+          <p className="my-5">
+            Searching for: <span id="search-term">{searchTerm || "Nothing yet"}</span>
+          </p>
+          <form action={handleSearch} className="flex flex-row gap-x-4">
+            <input className="search" placeholder="Search for Advocates" value={inputValue} onChange={onChange} />
+            {/* create component library for project */}
+            <button type="reset" onClick={handleReset}>Reset</button>
+            <button type="submit"> Search</button>
+          </form>
+        </div>
+        <Card>
+          <CardHeader>
+            <CardTitle>Advocates</CardTitle>
+            <CardDescription>
+              View advocate search results and related information.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>First Name</TableHead>
+                  <TableHead>Last Name</TableHead>
+                  <TableHead>City</TableHead>
+                  <TableHead>Degree</TableHead>
+                  <TableHead>Specialties</TableHead>
+                  <TableHead>Years of Experience</TableHead>
+                  <TableHead>Phone Number</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {advocates.map((advocate) => {
+                  return (
+                    <TableRow key={advocate.id}>
+                      <TableCell>{advocate.firstName}</TableCell>
+                      <TableCell>{advocate.lastName}</TableCell>
+                      <TableCell>{advocate.city}</TableCell>
+                      <TableCell>{advocate.degree}</TableCell>
+                      <TableCell>
+                        {advocate.specialties.map((s: string) => (
+                          <div key={s}>{s}</div>
+                        ))}
+                      </TableCell>
+                      <TableCell>{advocate.yearsOfExperience}</TableCell>
+                      <TableCell>{advocate.phoneNumber}</TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </CardContent>
+        </Card>
+      </div>
     </main>
   );
 }
